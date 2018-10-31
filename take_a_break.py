1bfd6d279fd042aa1784c4847c7af4706f15d751
sarleone <sarah.m.leone@gmail.com>
2018-10-31 13:18:06 -0500

    first commit

---
+++ take_a_break.py
@@ -0,0 +1,11 @@
+import time 
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+
+print("This program started on " + time.ctime())
+while (break_count < total_breaks):
+    time.sleep(7200)
+    webbrowser.open("https://www.youtube.com/watch?v=EtH9Yllzjcc")
+    break_count = break_count + 1
